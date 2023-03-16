7974bcc25b5b9d092b0396638ae1c908241d9b59
RomanBlinnikov <rom.b@inbox.lv>
2023-03-16 16:23:41 +0200

    homework

---
+++ 09.03_FE/script.js
@@ -0,0 +1,71 @@
+
+// Task 1
+// let number1 = prompt("insert number 1")// recive number 1 
+// let number2 = prompt("insert number 2")// recive number 2
+// let number3 = prompt("insert number 3")// recive number 3
+// let maxValue = Math.max(number1, number2, number3)
+ // find maximum value
+//  document.write(maxValue)// print maximum value
+//  ======================================================
+
+
+/*
+Task 2
+let randomNumber = prompt("insert value 0-5")
+let value = Number(randomNumber)
+
+switch (value) {
+    case 0:
+    document.write('Ноль')
+      break;
+    case 1:
+    document.write('Один')
+      break;
+    case 2:
+    document.write('Два')
+    break;
+    case 3:
+    document.write('Три')
+      break;
+      case 4:
+    document.write('Четыре')
+      break;
+      case 5:
+    document.write('Пять')
+      break;
+    default:
+    document.write('Введите число от 0 до 5');
+}*/
+
+// Task 3
+
+// let age = 5;
+
+// if (age < 10) {
+  // console.log('hi!');
+// } else if (age < 20) {
+  // console.log('hello!');
+  // console.log('hello!')} else {
+  // console.log('good day!');
+// }
+  
+/*(age = 10;
+age<10&&age<20?console.log('hi!')&&console.log('hello!'): console.log('hello!');
+*/
+
+// Task 4
+/*
+let number1 = prompt("insert a")
+let number2 = prompt("insert b")
+let result = prompt("isert a*b")
+let correctResult=Number(result)
+let correctAnswer=number1*number2
+if(correctResult===correctAnswer)
+{
+alert("You are right!")
+}
+else
+{
+alert("Try once more")
+}
+*/
